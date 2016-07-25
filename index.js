7cfe9ce6cb7dde107b785201bc76c8d6ed24f675
Tristan Foureur <tristan.foureur@gmail.com>
2016-07-24 22:40:36 -0700

    Adds an index file that exposes internal components

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    CommandRunner : require('./lib/commandrunner'),
+    OpsWorks : require('./lib/Opsworks'),
+    StackList : require('./lib/StackList'),
+    Prompt : require('./lib/prompt'),
+    Logger : require('./lib/logger')
+}
